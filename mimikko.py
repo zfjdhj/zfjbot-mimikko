25d797d7ccbe7c9d526a5075ada6eaadd7c6cf66
zfjdhj <320336328@qq.com>
2020-12-13 17:43:30 +0800

    server

--- mimikko.py
+++ mimikko.py
@@ -7,8 +7,9 @@
 from hoshino import *
 from nonebot import *
 from hoshino import Service
-
+from .mimikkoAutoSignIn.mimikko import mimikko,timeStamp2time
 import requests
+import json
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
@@ -16,85 +17,87 @@ sv = Service('zfjbot-mimikko',enable_on_default=False)
 group_id="426770092"
 bot=get_bot()
 
-app_id = 'wjB7LOP2sYkaMGLC'
-Version='3.1.2'
-Authorization='c5d8dba1-eaae-4fd9-8112-6759ab8a8c34'
-
-apiPath = 'http://api1.mimikko.cn/client/user/GetUserSignedInformation'
-# apiPath1 = 'http://api1.mimikko.cn/client/user/LoginWithPayload'
-apiPath2 = 'http://api1.mimikko.cn/client/dailysignin/log/30/0'
-# post_data = {"password": "d8a598c998d82c1943cd719dff2627a6ce728ce1a8c5b15746f066b07ce00ac9", "id": "320336328@qq.com"}
-sign_path='https://api1.mimikko.cn/client/RewardRuleInfo/SignAndSignInformationV3'
-
-def GetUserSignedInformation(url, app_id):
-    headers_get = {
-        'Cache-Control': 'Cache-Control:public,no-cache',
-        'Accept-Encoding': 'gzip',
-        'User-Agent': 'Mozilla/5.0(Linux;Android6.0.1;MuMu Build/V417IR;wv)AppleWebKit/537.36(KHTML,'
-                      'like Gecko)Version/4.0 Chrome/52.0.2743.100MobileSafari / 537.36',
-        'AppID': app_id,
-        'Version': '3.1.2',
-        'Authorization': Authorization,
-        'Connection': 'Keep-Alive',
-        'Host': 'api1.mimikko.cn'
-    }
-    headers_post = {
-        'Accept': 'application/json',
-        'Cache-Control': 'no-cache',
-        'AppID': app_id,
-        'Version': '3.1.2',
-        'Content-Type': 'application/json',
-        'Host': 'api1.mimikko.cn',
-        'Connection': 'Keep-Alive',
-        'Accept-Encoding': 'gzip',
-        'User-Agent': 'okhttp/3.12.1',
-    }
-
-    try:
-        with requests.get(url, headers=headers_get, verify=False,timeout=300) as resp:
-            res = resp.text
-            return res
-
-    except Exception as ex:
-        return ex
-        print(ex)
 
 
 @sv.on_fullmatch('mimikko check')
 async def mimikko_check(bot, ev):
-    login_data = GetUserSignedInformation(apiPath, app_id)
-    print(login_data)
-    login_history = GetUserSignedInformation(apiPath2, app_id)
-    print(login_history)
-
-    await bot.send(ev, login_data)
-    await bot.send(ev, login_history)
-
+    sign_data, energy_info_data, energy_reward_data, sign_info, sign_history = mimikko()
+    res="Sign Data:\n"
+    res +=f"code: {sign_data['code']}\n"
+    res +=f"获得成长值Reward：{sign_data['body']['Reward']}\n"
+    res +=f"获得硬币GetCoin：{sign_data['body']['GetCoin']}\n"
+    res +=f"================\nEnergy Info:\n"
+    res +=f"code: {energy_info_data['code']}\n"
+    res +=f"msg: {energy_info_data['msg']}\n"
+    res +=f"经验：{energy_info_data['body']['Favorability']}/{energy_info_data['body']['MaxFavorability']}\n"
+    res+=f"Energy: {energy_info_data['body']['Energy']}\n"
+    res+="================\nEnergy Reward:\n"
+    res+=f"{energy_reward_data}\n"
+    res+="================\nSign Info:\n"
+    res +=f"code: {sign_info['code']}\n"
+    res +=f"IsSign: {sign_info['body']['IsSign']}\n"
+    res +=f"连续登录天数: {sign_info['body']['ContinuousSignDays']}\n"
+    res +="================\nSign History:\n"
+    res +=f"code: {sign_history['code']}\n"
+    res +=f"startTime: {timeStamp2time(sign_history['body']['startTime'])}\n"
+    res +=f"endTime: {timeStamp2time(sign_history['body']['endTime'])}\n"
+    res +='signLogs:'
+    for item in sign_history['body']['signLogs']:
+        res +=f"\n{timeStamp2time(item['signDate'])}"
+    await bot.send(ev,res)
 
 @sv.on_fullmatch('mimikko sign')
 async def mimikko_sign_in(bot, ev):
-    sign_data = GetUserSignedInformation(sign_path, app_id)
-    print(sign_data)
-    login_data = GetUserSignedInformation(apiPath2, app_id)
-    print(login_data)
-
-    # await bot.send(ev, login_data)
-    # await bot.send(ev, login_history)
-    
-    await bot.send_group_msg(group_id=group_id,message=sign_data)
-    await bot.send_group_msg(group_id=group_id,message=login_data)
+    sign_data, energy_info_data, energy_reward_data, sign_info, sign_history = mimikko()
+    res="Sign Data:\n"
+    res +=f"code:, {sign_data['code']}\n"
+    res +=f"获得成长值Reward：{sign_data['body']['Reward']}\n"
+    res +=f"获得硬币GetCoin：{sign_data['body']['GetCoin']}\n"
+    res +=f"================\nEnergy Info:\n"
+    res +=f"code: {energy_info_data['code']}\n"
+    res +=f"msg: {energy_info_data['msg']}\n"
+    res +=f"经验：{energy_info_data['body']['Favorability']}/{energy_info_data['body']['MaxFavorability']}\n"
+    res+=f"Energy: {energy_info_data['body']['Energy']}"
+    await bot.send(ev, res)
+   
+   
+@sv.on_fullmatch('mimikko energy')
+async def mimikko_sign_in(bot, ev):
+    sign_data, energy_info_data, energy_reward_data, sign_info, sign_history = mimikko()
+    res =f'Energy Info:\n'
+    res +=f"code: {energy_info_data['code']}\n"
+    res +=f"msg: {energy_info_data['msg']}\n"
+    res +=f"经验：{energy_info_data['body']['Favorability']}/{energy_info_data['body']['MaxFavorability']}\n"
+    res+=f"Energy: {energy_info_data['body']['Energy']}\n"
+    res+="================\nEnergy Reward:\n"
+    res+=f"{energy_reward_data}\n"
+    await bot.send(ev, res)
 
 async def mimikko_sign_in_auto():
-    sign_data = GetUserSignedInformation(sign_path, app_id)
-    print(sign_data)
-    login_data = GetUserSignedInformation(apiPath2, app_id)
-    print(login_data)
-
-    # await bot.send(ev, login_data)
-    # await bot.send(ev, login_history)
-    
-    await bot.send_group_msg(group_id=group_id,message=sign_data)
-    await bot.send_group_msg(group_id=group_id,message=login_data)
+    sign_data, energy_info_data, energy_reward_data, sign_info, sign_history = mimikko()
+    res="Sign Data:\n"
+    res +=f"code:, {sign_data['code']}\n"
+    res +=f"获得成长值Reward：{sign_data['body']['Reward']}\n"
+    res +=f"获得硬币GetCoin：{sign_data['body']['GetCoin']}\n"
+    res +=f"================\nEnergy Info:\n"
+    res +=f"code: {energy_info_data['code']}\n"
+    res +=f"msg: {energy_info_data['msg']}\n"
+    res +=f"经验：{energy_info_data['body']['Favorability']}/{energy_info_data['body']['MaxFavorability']}\n"
+    res+=f"Energy: {energy_info_data['body']['Energy']}\n"
+    res+="================\nEnergy Reward:\n"
+    res+=f"{energy_reward_data}\n"
+    res+="================\nSign Info:\n"
+    res +=f"code: {sign_info['code']}\n"
+    res +=f"IsSign: {sign_info['body']['IsSign']}\n"
+    res +=f"连续登录天数: {sign_info['body']['ContinuousSignDays']}\n"
+    res +="================\nSign History:\n"
+    res +=f"code: {sign_history['code']}\n"
+    res +=f"startTime: {timeStamp2time(sign_history['body']['startTime'])}\n"
+    res +=f"endTime: {timeStamp2time(sign_history['body']['endTime'])}\n"
+    res +='signLogs:'
+    for item in sign_history['body']['signLogs']:
+        res +=f"\n{timeStamp2time(item['signDate'])}"
+    await bot.send_group_msg(group_id=group_id,message=res)
 
 
 scheduler.add_job(mimikko_sign_in_auto, 'cron',hour='12')
